5098731e6b2a03d9a897b9ddb72bc88385f96315
aps0047 <aps0047@alu.ubu.es>
2016-05-26 23:02:06 +0200

    #1 Extract method 'loggingNodo(Writer report, Node currentNode)' from 'lanSimulation.Network.brequestBroadcast' to 'lanSimulation.Network'

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -173,13 +173,7 @@ which should be treated by all nodes.
 		Packet packet = new Packet("BROADCAST", firstNode_.name_, firstNode_.name_);
 		do {
 			try {
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' accepts broadcase packet.\n");
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
-				report.flush();
+				loggingNodo(report, currentNode);
 			} catch (IOException exc) {
 				// just ignore
 			};
@@ -192,6 +186,15 @@ which should be treated by all nodes.
 			// just ignore
 		};
 		return true;
+	}
+
+	
+
+	private void loggingNodo(Writer report, Node currentNode) throws IOException {
+		report.write("\tNode '");
+		report.write(currentNode.name_);
+		report.write("' passes packet on.\n");
+		report.flush();
 	}    
 
 	/**
@@ -228,10 +231,7 @@ Therefore #receiver sends a packet across the token ring network, until either
 		startNode = (Node) workstations_.get(workstation);
 
 		try {
-			report.write("\tNode '");
-			report.write(startNode.name_);
-			report.write("' passes packet on.\n");
-			report.flush();
+			loggingNodo(report, startNode);
 		} catch (IOException exc) {
 			// just ignore
 		};
@@ -239,10 +239,7 @@ Therefore #receiver sends a packet across the token ring network, until either
 		while ((! packet.destination_.equals(currentNode.name_))
 				& (! packet.origin_.equals(currentNode.name_))) {
 			try {
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
-				report.flush();
+				loggingNodo(report, currentNode);
 			} catch (IOException exc) {
 				// just ignore
 			};
