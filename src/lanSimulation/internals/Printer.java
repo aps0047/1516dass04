b16c53adaede300f3775df21780fd10361ffea53
are0012 <are0012@alu.ubu.es>
2016-05-30 20:13:08 +0200

    #3 Subclases vacías WorkStation y Printer creadas

---
+++ src/lanSimulation/internals/Printer.java
@@ -0,0 +1,10 @@
+package lanSimulation.internals;
+
+public class Printer extends Node {
+
+	public Printer(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
